804032995de59f862e9bc318d1cf66c4e6e5d62f
EC2 Default User <ec2-user@ip-172-31-6-100.ap-northeast-1.compute.internal>
2025-05-20 16:32:15 +0000

    Initial commit for Devops CI/CD project

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Enable Docker on boot
+sudo systemctl enable docker
+
